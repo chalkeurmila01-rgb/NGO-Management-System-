e5e180c8d6303a481e6824bb5ac5b4fd2ae099fe
chalkeurmila01-rgb <chalkeurmila01@gmail.com>
2026-08-12 00:10:29 +0530

    Implement user login with Firebase authentication
    Added Firebase authentication functionality for user login.

--- auth.js
+++ auth.js
@@ -1,10 +1,46 @@
-
 // =========================================
 // NGO Management System
 // Authentication
 // =========================================
 
-// Firebase Authentication functions
-// will be added later.
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
 
-console.log("Authentication file loaded.");
+import { auth } from "./firebase.js";
+
+// Login Form
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+
+    loginForm.addEventListener("submit", async (event) => {
+
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        try {
+
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+
+            console.log("Login successful!");
+            console.log("User:", userCredential.user);
+
+            alert("Login successful!");
+
+            window.location.href = "dashboard.html";
+
+        } catch (error) {
+
+            console.error("Login error:", error);
+
+            alert("Login failed: " + error.message);
+        }
+    });
+}
